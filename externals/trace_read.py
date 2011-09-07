d49a0f336c4fd4f6a75f56eb78135e1916b50253
jeff <leterip@gmail.com>
2011-09-07 11:23:14 -0400

    Some python code cleanup

--- externals/trace_read.py
+++ externals/trace_read.py
@@ -2,38 +2,37 @@
 
 import argparse
 import json
+from itertools import ifilter
 
-def trace_load(my_file):
+def should_keep(line):
+	if 'Version:' in line:
+		return False
+	if 'File format:' in line:
+		return False
+	if line.strip() == '':
+		return False
+	return True
+
+def trace_load(filename):
 	row_data = {}
 	lines = []
-	file = open(my_file, "r")
-	for line in file:
-		if 'Version:' in line:
-			pass
-		elif 'File format:' in line:
-			pass
-		elif line.strip() == '':
-			pass
-		elif 'TRACE START' in line:
-			row_data['start'] = line.strip()
-		elif 'TRACE END'in line:
-			row_data['end'] = line.strip()
-		else:
-			line_data = []
-			for x in line.split("\t"):
-		 		line_data.append (x.strip())
-		 	lines.append (line_data)
+	with open(filename) as f:
+		for line in ifilter(should_keep, f):
+			if 'TRACE START' in line:
+				row_data['start'] = line.strip()
+			elif 'TRACE END'in line:
+				row_data['end'] = line.strip()
+			else:
+				lines.append([x.strip() for x in line.split("\t")])
 	row_data['lines'] = lines
 	return json.dumps(row_data)
 
-def main():
+if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Loads and parses PHP XDebug trace files.')
-	parser.add_argument('file', metavar='F', nargs='?', help='The full path and filename of the tracefile to parse')
+	parser.add_argument('file', help='The full path and filename of the tracefile to parse')
 
 	args = parser.parse_args()
 	if args.file != None:
 		print trace_load(args.file)
 	else:
 		parser.print_help()
-
-main()
